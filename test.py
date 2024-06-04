d35bb61c8eede9350823c452c5fb230faa1d9400
dfgqwe <qkrwlsdn0424@naver.com>
2024-06-04 23:54:43 +0900

    Add files via upload

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pyperclip
+import base64
 import re
 
 # 포맷 데이터 포함
@@ -19,7 +20,6 @@ formats = {
     "차단기 on": "[사설차단기복구]",
     "trip": "[사설차단기복구]",
     "TRIP": "[사설차단기복구]",
-    "Trip": "[사설차단기복구]",
     "트립": "[사설차단기복구]",
     "어댑터": "[전원어댑터교체]",
     "어뎁터": "[전원어댑터교체]",
@@ -58,7 +58,46 @@ formats = {
     "익일": "[기타]",
     "담당조": "[기타]",
     "OFF": "[기타]",
-    "VOC": "[기타]",
+    "예정": "[기타]",
+    "재발행": "[기타]",
+    "VOC": "[기타]"
+}
+
+# B/S 및 민원처리 head_format 데이터
+B_S_head_formats = {
+    "NOC_광레벨불": "[NOC_광레벨불]",
+    "NOC_CRC발생": "[NOC_CRC발생]",
+    "NOC_장비철거": "[NOC_장비철거]",
+    "NOC_형상삭제": "[NOC_형상삭제]",
+    "NOC_PSU교체": "[NOC_PSU교체]",
+    "NOC_PLK_PSU교체": "[NOC_PLK_PSU교체]",
+    "NOC_장비교체": "[NOC_장비교체]",
+    "NOC_점검정비": "[NOC_점검정비]",
+    "NOC_자산관리": "[NOC_자산관리]",
+    "NOC_중복장애": "[NOC_중복장애]",
+    "NOC_kernel정비": "[NOC_kernel정비]"
+}
+
+민원처리_head_formats = {
+    "NOC_장비교체": "[NOC_장비교체]",
+    "NOC_PON모듈교체": "[NOC_PON모듈교체]",
+    "NOC_보드교체": "[NOC_보드교체]",
+    "NOC_BAT교체": "[NOC_BAT교체]",
+    "NOC_어댑터교체": "[NOC_어댑터교체]",
+    "NOC_FAN교체": "[NOC_FAN교체]",
+    "NOC_관련부서이관": "[NOC_관련부서이관]",
+    "NOC_장비철거": "[NOC_장비철거]",
+    "NOC_광커넥터재접속": "[NOC_광커넥터재접속]",
+    "NOC_전원가복구": "[NOC_전원가복구]",
+    "NOC_모자분리": "[NOC_모자분리]",
+    "NOC_전기요금": "[NOC_전기요금]",
+    "NOC_장비재설치": "[NOC_장비재설치]",
+    "NOC_PSU교체": "[NOC_PSU교체]",
+    "NOC_감쇄기실장": "[NOC_감쇄기실장]",
+    "NOC_상태변경": "[NOC_상태변경]",
+    "NOC_장비리셋": "[NOC_장비리셋]",
+    "NOC_자연회복": "[NOC_자연회복]",
+    "NOC_기타": "[NOC_기타]"
 }
 
 # 선조치_NOC에 대한 내용
@@ -84,6 +123,8 @@ def get_format(text):
     return None
 
 def main():
+    
+
     st.title("MOSS 회복 문구")
 
     # 초기값 설정
@@ -98,13 +139,36 @@ def main():
 
     results = []
 
+    col1, col2 = st.columns(2)
+
+    # B/S 체크박스 추가
+    with col1:
+        is_bs_checked = st.checkbox("B/S")
+
+    # 민원처리 체크박스 추가
+    with col2:
+        is_complaint_checked = st.checkbox("민원처리")
+
+    if is_bs_checked:
+        # B/S 체크박스가 선택되었을 때 B/S head_format을 선택할 수 있는 드롭다운 메뉴
+        selected_bs_format = st.selectbox("B/S head_format을 선택하세요:", list(B_S_head_formats.values()), key="bs_format")
+        if selected_bs_format:
+            results.append(selected_bs_format)
+
+    if is_complaint_checked:
+        # 민원처리 체크박스가 선택되었을 때 민원처리 head_format을 선택할 수 있는 드롭다운 메뉴
+        selected_complaint_format = st.selectbox("민원처리 head_format을 선택하세요:", list(민원처리_head_formats.values()), key="complaint_format")
+        if selected_complaint_format:
+            results.append(selected_complaint_format)
+
     # 텍스트 입력 위젯
     user_input = st.text_input("입력란", key="user_input")
 
     # 입력 형식 검사 및 포맷 추가
-    head_format = get_format(user_input)
-    if head_format:
-        results.append(head_format)
+    if not is_bs_checked and not is_complaint_checked:
+        head_format = get_format(user_input)
+        if head_format:
+            results.append(head_format)
 
     results.append(user_input)
     results.append("수고하셨습니다")
@@ -136,16 +200,19 @@ def main():
         else:
             formatted_TM = f"[현장TM] {현장TM_내용}"  # 포함되어 있지 않으면 [현장TM] 추가
         selected_locations.remove("[현장TM]")
-        formatted_locations = f"{formatted_TM}, " + " / ".join([f"{location}" if location != "기타(간단히 내용입력)" else f"기타({st.text_input('기타 내용 입력', key='기타_내용')})" for location in selected_locations])
+        if selected_locations:
+            formatted_locations = f"{formatted_TM}, " + " / ".join([f"{location}" if location != "기타(간단히 내용입력)" else f"기타({st.text_input('기타 내용 입력', key='기타_내용')})" for location in selected_locations]) + " 수정요청"
+        else:
+            formatted_locations = f"{formatted_TM}"
     else:
         formatted_locations = " / ".join([f"{location}" if location != "기타(간단히 내용입력)" else f"기타({st.text_input('기타 내용 입력', key='기타_내용')})" for location in selected_locations])
     
-    if selected_locations or "[현장TM]" in locals():
-        results.append(f"<현장> {formatted_locations} 수정요청")
+    if selected_locations or 현장TM_내용:
+        results.append(f"<현장> {formatted_locations}")
 
     출동예방_actions = []
     # [현장TM] 내용에 특정 키워드가 포함되거나 숫자나 한국 전화번호 형태가 있는 경우 <출동예방>에도 추가
-    if 현장TM_내용 and (any(keyword in 현장TM_내용 for keyword in ["연락", "전화", "건물주", "확인", "통화"]) or re.search(r"\d", 현장TM_내용) or re.search(r"\d{3}-\d{4}-\d{4}", 현장TM_내용)):
+    if 현장TM_내용 and (any(keyword in 현장TM_내용 for keyword in ["연락", "전화", "건물주", "통화"]) or re.search(r"\d", 현장TM_내용) or re.search(r"\d{3}-\d{4}-\d{4}", 현장TM_내용)):
         출동예방_actions.append(formatted_TM)
 
     # "전기작업 확인(전화)"가 선택된 경우 <출동예방>에도 추가
