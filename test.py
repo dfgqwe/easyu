9fa99d303783db972c944f7c8863460ee404053a
dfgqwe <qkrwlsdn0424@naver.com>
2024-06-05 23:00:27 +0900

    Add files via upload

--- test.py
+++ test.py
@@ -4,9 +4,6 @@ import re
 
 # 포맷 데이터 포함
 formats = {
-    "출입불가": "[폐문]",
-    "출입": "[폐문]",
-    "한전": "[한전정전복구]",
     "정전": "[사설정전복구]",
     "입전": "[사설정전복구]",
     "복전": "[사설정전복구]",
@@ -15,6 +12,7 @@ formats = {
     "출동 중 자동복구": "[사설정전복구]",
     "출동중자동복구": "[사설정전복구]",
     "자동": "[사설정전복구]",
+    "한전": "[한전정전복구]",
     "차단기 on": "[사설차단기복구]",
     "trip": "[사설차단기복구]",
     "TRIP": "[사설차단기복구]",
@@ -58,12 +56,15 @@ formats = {
     "익일": "[기타]",
     "담당조": "[기타]",
     "OFF": "[기타]",
+    "off": "[기타]",
     "예정": "[기타]",
     "재발행": "[기타]",
     "VOC": "[기타]",
+    "voc": "[기타]",
     "미정": "[기타]",
+    "출입불가": "[폐문]",
+    "출입": "[폐문]",
     "폐문": "[폐문]"
-    
 }
 
 # B/S 및 민원처리 head_format 데이터
@@ -120,10 +121,8 @@ B_S_head_formats = {
 ]
 
 def get_format(text):
-    for keyword, head_format in formats.items():
-        if keyword in text:
-            return head_format
-    return None
+    matched_formats = [head_format for keyword, head_format in formats.items() if keyword in text]
+    return matched_formats[-1] if matched_formats else None
 
 def main():
     st.title("MOSS 회복 문구")
@@ -140,32 +139,26 @@ def main():
 
     results = []
 
+    # B/S 및 민원처리 체크박스
     col1, col2 = st.columns(2)
 
-    # B/S 체크박스 추가
     with col1:
         is_bs_checked = st.checkbox("B/S")
-
-    # 민원처리 체크박스 추가
     with col2:
         is_complaint_checked = st.checkbox("민원처리")
 
     if is_bs_checked:
-        # B/S 체크박스가 선택되었을 때 B/S head_format을 선택할 수 있는 드롭다운 메뉴
         selected_bs_format = st.selectbox("B/S head_format을 선택하세요:", list(B_S_head_formats.values()), key="bs_format")
         if selected_bs_format:
             results.append(selected_bs_format)
 
     if is_complaint_checked:
-        # 민원처리 체크박스가 선택되었을 때 민원처리 head_format을 선택할 수 있는 드롭다운 메뉴
         selected_complaint_format = st.selectbox("민원처리 head_format을 선택하세요:", list(민원처리_head_formats.values()), key="complaint_format")
         if selected_complaint_format:
             results.append(selected_complaint_format)
 
-    # 텍스트 입력 위젯
     user_input = st.text_input("입력란", key="user_input")
 
-    # 입력 형식 검사 및 포맷 추가
     if not is_bs_checked and not is_complaint_checked:
         head_format = get_format(user_input)
         if head_format:
@@ -174,13 +167,11 @@ def main():
     results.append(user_input)
     results.append("수고하셨습니다")
 
-    # 선조치_NOC 다중 선택 박스
     selected_actions = st.multiselect("선조치_NOC에 대한 내용을 선택하세요:", 선조치_NOC_options, key="selected_actions")
     if selected_actions:
         formatted_actions = ", ".join(selected_actions)
         results.append(f"<선조치_NOC> {formatted_actions}")
 
-    # 현장 항목들 다중 선택 박스
     현장_options = [
         "[현장TM]",
         "주소",
@@ -192,15 +183,14 @@ def main():
     ]
     selected_locations = st.multiselect("현장에 대한 내용을 선택하세요:", 현장_options, key="selected_locations")
 
-    # [현장TM] 선택 시 입력란 추가
     현장TM_내용 = ""
     if "[현장TM]" in selected_locations:
         현장TM_내용 = st.text_input("[현장TM] 내용을 입력하세요:", key="현장TM_내용")
         현장TM_출동예방 = st.checkbox("[현장TM] 내용을 <출동예방>에 포함")
-        if "[현장TM]" in 현장TM_내용:
-            formatted_TM = 현장TM_내용  # [현장TM]이 포함되어 있으면 그대로 사용
+        if "[TM활동]" in 현장TM_내용:
+            formatted_TM = 현장TM_내용.replace("[현장TM]", "[TM활동]")
         else:
-            formatted_TM = f"[현장TM] {현장TM_내용}"  # 포함되어 있지 않으면 [현장TM] 추가
+            formatted_TM = f"[현장TM] {현장TM_내용}"
         selected_locations.remove("[현장TM]")
         if selected_locations:
             formatted_locations = f"{formatted_TM}, " + " / ".join([f"{location}" if location != "기타(간단히 내용입력)" else f"기타({st.text_input('기타 내용 입력', key='기타_내용')})" for location in selected_locations]) + " 수정요청"
@@ -210,33 +200,28 @@ def main():
         formatted_locations = " / ".join([f"{location}" if location != "기타(간단히 내용입력)" else f"기타({st.text_input('기타 내용 입력', key='기타_내용')})" for location in selected_locations])
         if selected_locations:
             formatted_locations += " 수정요청"
-    
+
     if selected_locations or 현장TM_내용:
         results.append(f"<현장> {formatted_locations}")
 
     출동예방_actions = []
-    # [현장TM] 내용에 특정 키워드가 포함되거나 숫자나 한국 전화번호 형태가 있는 경우 <출동예방>에도 추가
     if 현장TM_내용 and 현장TM_출동예방:
         출동예방_actions.append(formatted_TM)
-
-    # "전기작업 확인(전화)"가 선택된 경우 <출동예방>에도 추가
     if "전기작업 확인(전화)" in selected_actions:
         출동예방_actions.append("[NOC]전기작업 확인(전화)")
-
     if "출동보류" in selected_actions:
         출동예방_actions.append("[NOC]출동보류")
-    
+
     if 출동예방_actions:
         results.insert(3, f"<출동예방>{', '.join(출동예방_actions)}")
 
     copy_activated = False
     if st.button("출력"):
-        output_text = "\n".join(results)
+        output_text = "\n".join(results)  # Join results with new lines for the desired format
+        st.text(output_text)  # Print output_text when the "출력" button is pressed
         if copy_activated:
             pyperclip.copy(output_text)
-        st.text(output_text)
 
-    # 입력란 초기화 버튼
     if st.button("입력란 초기화"):
         clear_text()
 
