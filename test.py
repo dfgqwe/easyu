8d96f28a7d5813aef765680b7a9720ed5cfa7ae8
dfgqwe <qkrwlsdn0424@naver.com>
2024-06-13 23:05:37 +0900

    test.py

--- test.py
+++ test.py
@@ -153,7 +153,14 @@ st.sidebar.title("Menu")
 # Expander in sidebar
 with st.sidebar.expander('MOSS 회복 항목 표준'):
     st.dataframe(df)
-    sidebar_expanded = st.sidebar.expander('Menu')._visible
+# 초기값 설정
+if "sidebar_expanded" not in st.session_state:
+    st.session_state.sidebar_expanded = False
+
+# Streamlit 애플리케이션
+with st.sidebar.expander('Menu'):
+    # expander를 클릭하여 확장 또는 축소될 때마다 상태 업데이트
+    st.session_state.sidebar_expanded = st.sidebar.expander('Menu')._visible
 
 
 def clear_tm_content(content):
