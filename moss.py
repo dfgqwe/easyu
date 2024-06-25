72633f901769a1bf0261ab2114286f2cd5157a4f
dfgqwe <qkrwlsdn0424@naver.com>
2024-06-25 22:01:46 +0900

    moss.py

--- moss.py
+++ moss.py
@@ -156,7 +156,13 @@ B_S_head_formats = {
     "DB 삭제 여부"
 ]
 
-
+client_id = st.secrets["google"]["client_id"]
+project_id = st.secrets["google"]["project_id"]
+auth_uri = st.secrets["google"]["auth_uri"]
+token_uri = st.secrets["google"]["token_uri"]
+auth_provider_x509_cert_url = st.secrets["google"]["auth_provider_x509_cert_url"]
+client_secret = st.secrets["google"]["client_secret"]
+redirect_uris = st.secrets["google"]["redirect_uris"]
 
 
 @st.cache_data
