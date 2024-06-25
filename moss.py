f7ed112584b581ac408dce09a0230fe8a85f0ad8
dfgqwe <qkrwlsdn0424@naver.com>
2024-06-25 22:11:27 +0900

    moss.py

--- moss.py
+++ moss.py
@@ -213,9 +213,16 @@ def load_data_from_google_drive(file_id):
     client_secret = st.secrets["installed"]["client_secret"]
     redirect_uris = st.secrets["installed"]["redirect_uris"]
     
-    credentials = service_account.Credentials.from_service_account_info(
-        st.secrets["client_id "]
-    )
+    credentials = service_account.Credentials.from_service_account_info({
+        "client_id": client_id,
+        "project_id": project_id,
+        "auth_uri": auth_uri,
+        "token_uri": token_uri,
+        "auth_provider_x509_cert_url": auth_provider_x509_cert_url,
+        "client_secret": client_secret,
+        "redirect_uris": redirect_uris
+    })
+    
     service = build('drive', 'v3', credentials=credentials)
     request = service.files().get_media(fileId=file_id)
     file_content = io.BytesIO(request.execute())
