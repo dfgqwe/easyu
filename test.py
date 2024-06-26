02c4fb51f01efb1c459c611284dd69f80ad8863a
dfgqwe <qkrwlsdn0424@naver.com>
2024-06-26 10:18:52 +0900

    test.py

--- test.py
+++ test.py
@@ -256,14 +256,7 @@ def home_page():
 
 
 
-
-
-
-
-
-
 def moss_page():
-
     st.title("MOSS 회복 문구")
 
     df1 = pd.read_csv('bs_head.csv')
@@ -284,10 +277,8 @@ def moss_page():
         st.session_state.clear()  # 모든 상태를 초기화
         st.session_state.user_input = ""  # 다시 설정
         st.experimental_rerun()  # 상태를 초기화하고 재실행
-        
 
     results = []
-
 
     if "bs_checked" not in st.session_state:
         st.session_state.bs_checked = False
@@ -299,7 +290,7 @@ def moss_page():
 
     def complaint_checkbox_callback():
         st.session_state.bs_checked = False
-    
+
     # B/S 및 민원처리 체크박스
     col1, col2 = st.columns(2)
 
@@ -312,6 +303,27 @@ def moss_page():
         selected_bs_format = st.selectbox("B/S head_format을 선택하세요:", list(B_S_head_formats.values()), key="bs_format")
         if selected_bs_format:
             results.append(selected_bs_format)
+
+            # Check if selected format is "[NOC_광레벨불]"
+            if selected_bs_format == "[NOC_광레벨불]":
+                cm_transfer = st.checkbox("CM팀 이관")
+                improvement = st.checkbox("개선")
+                not_fixed = st.checkbox("정비 안됨")
+
+                if cm_transfer:
+                    results.append("CM팀 이관")
+                if improvement:
+                    results.append("개선")
+                if not_fixed:
+                    results.append("정비 안됨")
+
+                ddm_value = st.text_input("ddm 값을 입력하세요:")
+                rssi_value = st.text_input("RSSI 값을 입력하세요:")
+
+                if ddm_value:
+                    results.append(f"ddm: {ddm_value}")
+                if rssi_value:
+                    results.append(f"RSSI: {rssi_value}")
 
     if is_complaint_checked:
         selected_complaint_format = st.selectbox("민원처리 head_format을 선택하세요:", list(민원처리_head_formats.values()), key="complaint_format")
@@ -326,7 +338,6 @@ def moss_page():
             results.append(head_format)
 
     results.append(user_input)
-
 
     출동예방_actions = []
     selected_actions = st.multiselect("선조치_NOC에 대한 내용을 선택하세요:", 선조치_NOC_options, key="selected_actions")
@@ -344,20 +355,18 @@ def moss_page():
 
         def 기타_neoss_완료_callback():
             st.session_state.기타_고객DB_neoss_불가 = False
-        
+
         기타_고객DB_neoss_불가 = st.checkbox("고객DB 존재 NeOSS 삭제 불가", key="기타_고객DB_neoss_불가", on_change=기타_고객DB_neoss_불가_callback)
         기타_neoss_완료 = st.checkbox("NeOSS 삭제 완료", key="기타_neoss_완료", on_change=기타_neoss_완료_callback)
-    
+
         if 기타_고객DB_neoss_불가:
             기타_results.append("고객DB 존재/NeOSS 삭제 불가")
         if 기타_neoss_완료:
             기타_results.append("NeOSS 삭제 완료")
 
-
     results.extend(기타_results)
     results.append("수고하셨습니다")
 
-    
     filtered_actions = [action for action in selected_actions if action != "DB 삭제 여부"]
     if filtered_actions:
         formatted_actions = ", ".join(filtered_actions)
@@ -367,7 +376,6 @@ def moss_page():
         if "출동보류" in selected_actions:
             출동예방_actions.append("[NOC]출동보류")
 
-    
     현장_options = [
         "[현장TM]",
         "주소",
@@ -385,7 +393,7 @@ def moss_page():
         현장TM_출동예방 = st.checkbox("[현장TM] 내용을 <출동예방>에 포함")
         cleaned_TM_내용 = clear_tm_content(현장TM_내용)
         formatted_TM = f"[현장TM] {cleaned_TM_내용}" if cleaned_TM_내용 else "[현장TM]"
-        
+
         if len(selected_locations) > 1:  # selected_locations에 [현장TM] 이외의 항목이 포함된 경우에만 수정요청 추가
             formatted_locations = f"{formatted_TM}, " + " / ".join([f"{location}" if location != "기타(간단히 내용입력)" else f"기타({st.text_input('기타 내용 입력', key='기타_내용')})" for location in selected_locations if location != "[현장TM]"]) + " 수정요청"
         else:
@@ -404,11 +412,9 @@ def moss_page():
     if 출동예방_actions:
         results.insert(3, f"<출동예방>{', '.join(출동예방_actions)}")
 
-
     copy_activated = False
 
-
-    col1, col2 , col3= st.columns([2.8, 0.5, 0.7])
+    col1, col2, col3 = st.columns([2.8, 0.5, 0.7])
 
     with col1:
         if st.button("출력"):
@@ -416,7 +422,7 @@ def moss_page():
             st.text(output_text)  # Print output_text when the "출력" button is pressed
             if copy_activated:
                 pyperclip.copy(output_text)
-                
+
     with col2:
         if st.button("입력란 초기화"):
             clear_text()
@@ -424,7 +430,7 @@ def moss_page():
     with col3:
         if 'button_clicked' not in st.session_state:
             st.session_state['button_clicked'] = False
-            
+
         if st.button('MOSS 회복 항목 표준'):
             st.session_state['button_clicked'] = not st.session_state['button_clicked']
 
@@ -432,20 +438,26 @@ def moss_page():
             placeholder = st.empty()
             with placeholder.container():
                 st.markdown(
-            """
-            <style>
-            /* 데이터프레임이 최대한 화면에 가깝게 보이도록 스타일 조정 */
-            .css-1l02zno {
-                width: 100%;
-                max-width: 100%;
-                height: calc(100vh - 200px); /* 높이를 화면 높이의 일부분으로 설정 */
-                overflow: auto; /* 스크롤이 필요한 경우 스크롤 허용 */
-            }
-            </style>
-            """,
-            unsafe_allow_html=True
-        )
-                st.dataframe(df)
+                    """
+                    <style>
+                    /* 데이터프레임이 최대한 화면에 가깝게 보이도록 스타일 조정 */
+                    .css-1l02zno {
+                        width: 100%;
+                        max-width: 100%;
+                        height: calc(100vh - 200px); /* 높이를 화면 높이의 일부분으로 설정 */
+                        overflow: auto; /* 스크롤이 필요한 경우 스크롤 허용 */
+                    }
+                    </style>
+                    """,
+                    unsafe_allow_html=True
+                )
+                st.dataframe(df1_reset)
+
+
+
+
+
+
                 
 # Worksync 페이지
 def worksync_page():  
