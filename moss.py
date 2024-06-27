6cc124c4ab00c43de8158cb8d84ebc7ad405bb64
dfgqwe <qkrwlsdn0424@naver.com>
2024-06-27 22:36:02 +0900

    moss.py

--- moss.py
+++ moss.py
@@ -14,7 +14,7 @@ import io
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-from googleapiclient.http import MediaFileUpload
+from googleapiclient.http import MediaIoBaseUpload
 
 # 포맷 데이터 포멧
 formats = {
