bf17084a4433d335459cee9b17a00067672f17ce
dfgqwe <qkrwlsdn0424@naver.com>
2024-07-06 15:34:34 +0900

    test.py

--- test.py
+++ test.py
@@ -370,10 +370,12 @@ def moss_page():
 
     selected_bs_format = None
 
-    if is_bs_checked:
+  if is_bs_checked:
         selected_bs_format = st.selectbox("B/S head_format을 선택하세요:", list(B_S_head_formats.values()), key="bs_format")
         if selected_bs_format:
             results.append(selected_bs_format)
+
+            user_input = st.text_input("입력란", key="user_input")
 
             # Check if selected format is "[NOC_광레벨불]"
             if selected_bs_format == "[NOC_광레벨불]":
@@ -398,13 +400,11 @@ def moss_page():
 
                 rssi_value = st.text_input("RSSI 값을 입력하세요:")
                 ddm_value = st.text_input("ddm 값을 입력하세요:")
-                
 
                 if rssi_value:
                     results.append(f"RSSI: {rssi_value}")
                 if ddm_value:
                     results.append(f"ddm: {ddm_value}")
-                
 
             if selected_bs_format == "[NOC_장비철거]":
                 st.markdown(
@@ -425,6 +425,9 @@ def moss_page():
                 )
                 if selected_option:
                     results.append(selected_option)
+            
+            # User input is added right after the BS format
+            results.append(user_input)
                     
 
     
