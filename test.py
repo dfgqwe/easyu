b94e5a985d3ea74db4bc95ecc16c51ff886e4b5b
dfgqwe <qkrwlsdn0424@naver.com>
2024-07-18 23:21:22 +0900

    test.py

--- test.py
+++ test.py
@@ -436,12 +436,12 @@ def moss_page():
         df = pd.read_csv('국사.csv')
         branch_to_acceptance = {row['분기국사']: row['수용국사'] for _, row in df.iterrows()}
 
-        def get_station_names(input_station):
+        def get_station_names_with_nsc(input_station):
             original_input = input_station
             if input_station.endswith("국사"):
                 input_station = input_station[:-2]
     
-            matching_stations = df[df['분기국사'].str.contains(input_station)]
+            matching_stations = df[df['분기국사'].str.contains(input_station) | df['수용국사'].str.contains(input_station)]
             unique_branch_stations = matching_stations['분기국사'].unique()
             result_stations = []
             for branch_station in unique_branch_stations:
