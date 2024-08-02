92167f4f7d7d81e83162c79a42581f32aefc1527
dfgqwe <qkrwlsdn0424@naver.com>
2024-08-03 07:56:32 +0900

    test.py

--- test.py
+++ test.py
@@ -681,7 +681,7 @@ def moss_page():
             results.append(f"<출동예방>{', '.join(출동예방_actions)}")
 
         # 선조치_NOC 관련 결과 처리
-        filtered_actions = [action for action in selected_actions if action not in ["DB 삭제 여부", "광레벨 확인", "어댑터 교체"]]
+        filtered_actions = [action for action in selected_actions if action not in ["DB 삭제 여부", "광레벨 확인", "어댑터 전/후 작성"]]
         if filtered_actions:
             formatted_actions = ", ".join(filtered_actions)
             results.append(f"<선조치_NOC> {formatted_actions}")
