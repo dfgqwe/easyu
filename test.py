94d1d6378e67ab2c254f03ba4d2f4d77b7668aff
dfgqwe <qkrwlsdn0424@naver.com>
2024-08-06 11:21:39 +0900

    test.py

--- test.py
+++ test.py
@@ -694,20 +694,17 @@ def moss_page():
 
         # 현장 관련 처리
         formatted_locations = " / ".join([
-    f"{location}" if location != "기타(간단히 내용입력)"
-    else f"기타({st.text_input('기타 내용 입력', key='기타_내용')})"
-    for location in selected_locations
-    if location != "[현장TM]"
-])
+            f"{location}" if location != "기타(간단히 내용입력)"
+            else f"기타({st.text_input('기타 내용 입력', key='기타_내용')})"
+            for location in selected_locations
+            if location != "[현장TM]"
+        ])
 
-        if not 현장TM_내용:
-            formatted_locations = "[현장TM]"
-        else:
-            formatted_tm_content = f"[현장TM] {clear_tm_content(현장TM_내용)}"
-            if formatted_locations:
-                formatted_locations = f"{formatted_tm_content} , {formatted_locations.strip()}"
-            else:
-                formatted_locations = formatted_tm_content
+        if not 현장TM_출동예방 and 현장TM_내용:
+            formatted_locations = f"[현장TM] {clear_tm_content(현장TM_내용)}" + (f" , {formatted_locations.strip()} 수정요청" if formatted_locations else "")
+        elif formatted_locations:
+            formatted_locations = f"{formatted_locations.strip()} 수정요청"
+
         if formatted_locations:
             results.append(f"<현장> {formatted_locations}")
 
